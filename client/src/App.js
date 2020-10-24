0a8f893118f77b804b24823fb16d713fa5deef8b
Kali Persyn <ka1i@live.com>
2020-10-24 13:57:44 -0400

    Init commit

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+
+import logo from './images/poke-logo.png';
+import './App.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar bg="dark" variant="dark" expand="sm">
+            <Navbar.Brand href="/">
+              <img src={logo} className="App-logo d-inline-block align-top" width="30"
+                   height="30" alt="logo" />
+              {' '}
+              Poké Coach</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="about">About</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Container fluid className="main-container">
+            <Row>
+              <Col>
+                <div>
+                  <Switch>
+                    <Route exact path="/">
+                      <Home />
+                    </Route>
+                    <Route path="/about">
+                      <About />
+                    </Route>
+                  </Switch>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Home() {
+    const [sample, setSample] = React.useState(null);
+
+    const getSample = () => {
+        fetch('/api/sample')
+            .then(response => response.json())
+            .then(data => setSample(data));
+    };
+
+    return (
+        <div>
+            <button onClick={getSample}>Get sample data</button>
+            <h2>{sample && (
+                <div>
+                    {sample}
+                    </div>
+            )}</h2>
+        </div>
+    );
+}
+
+export default Home;
